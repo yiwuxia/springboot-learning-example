9610af0630f27e11a1dc963c55892818e4edad55
yiwuxia <740393778@qq.com>
2018-06-05 16:14:43 +0800

    lijin

---
+++ springboot-helloworld/src/main/java/org/spring/springboot/Application2.java
@@ -0,0 +1,17 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application2 {
+
+	
+
+	    public static void main(String[] args) {
+	        // 程序启动入口
+	        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+	        SpringApplication.run(Application2.class,args);
+	    }
+
+}
